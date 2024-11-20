5da24ffd0c013430c452e8d5f608de16605c3eda
2sandeep7 <ssandeep8696@gmail.com>
2024-11-20 22:01:15 +0530

    fix

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -9,7 +9,7 @@ interface ContainerProps {
 export default function Container({ children, bgColor }: ContainerProps) {
     return (
         <div
-            className={`container sm:my-28 my-9 mx-auto px-7 sm:px-10 lg:px-10 bg-[${bgColor}] relative  `}
+            className={`container sm:my-28 my-8 mx-auto px-7 sm:px-10 lg:px-10 bg-[${bgColor}] relative  `}
         >
             <div className="flex flex-col items-center gap-4 py-10 ">
                 {children}
